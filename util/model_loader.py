88ef15b1108d21fe51928e7b4d8d2f3bfc1b5c93
ystyuan <141152444+ystyuan@users.noreply.github.com>
2023-08-02 12:08:00 +0800

    Update model_loader.py

--- util/model_loader.py
+++ util/model_loader.py
@@ -1,11 +1,4 @@
-'''
-Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-Date: 2023-03-22 19:11:40
-LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-LastEditTime: 2023-03-27 14:54:53
-FilePath: /react-cifar-wrn/util/model_loader.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 import os
 
 
